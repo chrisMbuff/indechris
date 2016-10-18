b8f8c7cde1056596a06b3f97d33799be26927148
(s) Christopher Maycock <csmaycock@pm7-0098.uopnet.plymouth.ac.uk>
2016-10-18 16:55:01 +0100

    Kinda working now

---
+++ test1.py
@@ -0,0 +1,45 @@
+import random
+
+text_file = open("stopWord.txt", "r")
+stopwords = text_file.read().split('\n')
+stopwords.append("name")
+text_file.close()
+
+text_file = open("sad.txt", "r")
+sadWordsfile = text_file.read().split('\n')
+text_file.close()
+
+text_file = open("happy.txt", "r")
+happyWordsfile = text_file.read().split('\n')
+text_file.close()
+
+greetings = ['hola ', 'hello ', 'hi ', 'Hi ', 'hey! ','hey ', 'Wa Sup']
+random_greeting = random.choice(greetings)
+
+hairColour = ['Thats cool', 'That colour is fine', 'Look at you', 'sounds intersting']
+random_hair = random.choice(hairColour)
+
+
+
+
+nameQuestion = raw_input("What is your name ?")
+wordList = nameQuestion.split(" ")
+for word in wordList:
+    if word.lower() not in stopwords:
+        print (random_greeting + word)
+   
+        
+name = word        
+feelingQuestion = raw_input ('How are you feeling today? ')
+
+feelingWord = feelingQuestion.split(" ")
+for word in feelingWord:
+    if word.lower() == not:flip = True
+    elif word in happyWordsfile: emotion = "good"
+    elif word in sadWordsfile; emotion = "bad"
+    
+if flip and emotion ==
+        
+yourColour = raw_input("What is your hair colour ?")
+print(random_hair)
+
